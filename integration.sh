3a46f0c05f5723e461ccb2594cf41502c6ffa810
Frano Rajič <frano.rajich@gmail.com>
2019-10-31 14:46:32 +0100

    create powershell and bash integration script

---
+++ integration.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+dir_array=("svaki_drugi_a2" "svaki_drugi_a1" "simplePpjLang" "nadji_a1" "nadji_a2" "minusLang" "greske_u_stanju" "minusLang_laksi" "minusLang_tezi" "nadji_x" "nadji_x_oporavak" "nadji_x_retci" "poredak" "ppjLang_laksi" "ppjLang_tezi" "regex_escapes" "regex_laksi" "regex_regdefs" "regex_tezi" "simplePpjLang_laksi" "simplePpjLang_tezi" "state_hopper" "svaki_treci_x" "vrati_se" "vrati_se_prioritet" "duljina")
+
+
+time for dir in ${dir_array[*]}
+do
+    current="./data/lab1/tests/$dir/test"
+    echo $current
+    cat "$current.lan" | java -cp target/classes lexer.GLA
+    cat "$current.in" | java -cp target/classes lexer.LA > output.out
+    diff "$current.out" output.out
+done
+
+read -n 1 -p "Press any key to continue:" input
