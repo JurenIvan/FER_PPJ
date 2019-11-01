42807bc6cc84305cfc90e8c44ec6c739e51bf2b5
Frano Rajič <frano.rajich@gmail.com>
2019-11-01 17:37:57 +0100

    add expected test outputs

--- integration.sh
+++ integration.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
+# m43
 dir_array=("svaki_drugi_a2" "svaki_drugi_a1" "simplePpjLang" "nadji_a1" "nadji_a2" "minusLang" "greske_u_stanju" "minusLang_laksi" "minusLang_tezi" "nadji_x" "nadji_x_oporavak" "nadji_x_retci" "poredak" "ppjLang_laksi" "ppjLang_tezi" "regex_escapes" "regex_laksi" "regex_regdefs" "regex_tezi" "simplePpjLang_laksi" "simplePpjLang_tezi" "state_hopper" "svaki_treci_x" "vrati_se" "vrati_se_prioritet" "duljina")
 
 
