96f996cd58da583b7e9d16959bf85bf601ac4afc
Frano Rajič <frano.rajich@gmail.com>
2019-11-29 20:37:41 +0100

    Added first and follow impl (needed for context)

--- lab2/GSA.py
+++ lab2/GSA.py
@@ -1,3 +1,4 @@
+# fajl = open("test.txt")
 fajl = open("gramatikaPrimjer.txt")
 
 citaj = fajl.readlines()
@@ -85,38 +86,160 @@ for x in stanje:
 print(stanje)
 print(unikati)
 
+# def izravno_prazan(nezavrsni_znak):
+#     for produkcija in stanje_na_produkcije[nezavrsni_znak]:
+#         if produkcije_tuple[produkcija][1][0] == "$":
+#             return True
+#     return False
+#
+#
+# izravno_prazni_znakovi = {izravno_prazan(s) for s in nezavrsniZnakovi}
 
-def odredi_kontekst(ntorka):
-    # TODO
-    pass
+prazni_znakovi = set()
 
 
-def kontekst(alpha, parent_context):
-    # TODO
-    # ime bolje..
-    pass
+def je_li_znak_prazan(nezavrsni_znak, visited):
+    if (nezavrsni_znak in visited):
+        return nezavrsni_znak in prazni_znakovi
+
+    for produkcija in stanje_na_produkcije[nezavrsni_znak]:
+
+        if produkcije_tuple[produkcija][1][0] == "$":
+            prazni_znakovi.add(nezavrsni_znak)
+            break
+
+        for znak in produkcije_tuple[produkcija][1]:
+            if znak in zavrsniZnakovi:
+                break
+
+            if znak == nezavrsni_znak:
+                break  # TODO not sure if this is correct
+
+            if je_li_znak_prazan(znak, visited):
+                prazni_znakovi.add(nezavrsni_znak)
+                break
+
+    visited.add(nezavrsni_znak)
+    return nezavrsni_znak in prazni_znakovi
 
 
-# alpha je niz od nezavrsnih/zavrsnih znakova
+visited = set()
+for nezavrsni_znak in nezavrsniZnakovi:
+    if nezavrsni_znak not in visited:
+        je_li_znak_prazan(nezavrsni_znak, visited)
+
+
+# alpha je niz (lista) od nezavrsnih/zavrsnih znakova
 def prazan_skup(alpha):
+    # return true ako se alpha moze pretvoriti u $
+    # alpha smi biti prazan array
     if len(alpha) == 0:
         return True
 
     prvi = alpha[0]
 
-    if(zavrsniZnakovi):
-        pass
+    if prvi == "$":
+        return True
 
-    # TODO
-    # ako se alpha moze pretvoriti u $
-    # returna true false
-    pass
+    if prvi in zavrsniZnakovi:
+        return False
+
+    if prvi not in prazni_znakovi:
+        return False
+
+    return prazan_skup(alpha[1:])
 
 
-def zapocinje(alpha):
-    # TODO
-    # returna npr "a b @"
-    pass
+zapocinje = {}
+
+
+def init_zapocinje(nezavrsni_znak):
+    if nezavrsni_znak not in zapocinje:
+        zapocinje[nezavrsni_znak] = set()
+
+    for produkcija in stanje_na_produkcije[nezavrsni_znak]:
+        desna_strana = produkcije_tuple[produkcija][1]
+
+        for znak in desna_strana:
+            if znak == "$":
+                break
+
+            if znak in zavrsniZnakovi:
+                zapocinje[nezavrsni_znak].add(znak)
+                break
+
+            zapocinje[nezavrsni_znak].update(init_zapocinje(znak))
+            if znak not in prazni_znakovi:
+                break
+
+    return zapocinje[nezavrsni_znak]
+
+
+init_zapocinje(NNZ)
+for nezavrsni_znak in nezavrsniZnakovi:
+    if nezavrsni_znak not in zapocinje:
+        init_zapocinje(nezavrsni_znak)
+
+
+def zapocinje_alpha(alpha):
+    skup = set()
+
+    if len(alpha) == 0:
+        return skup
+
+    if alpha[0] in zavrsniZnakovi:
+        skup.add(alpha[0])
+    else:
+        skup.update(zapocinje[alpha[0]])
+        if (alpha[0] in prazni_znakovi):
+            skup.update(zapocinje_alpha(alpha[1:]))
+
+    return skup
+
+
+def odredi_kontekst_ntorke(ntorka):
+    # odredi kontekst prijelaza iza tocke
+    # na primjer za ntorku (A->aBc, 1, {x})
+    # mora vratiti set {c}
+    # a za ntorku (A->aB, 1 , {x})
+    # mora vratiti set {x}
+
+    desna_strana = produkcije_tuple[ntorka[0]][1]
+    assert (not ntorka[1] > len(desna_strana))  # nesmi tocka bit na kraju, nema smisla tada pozivati..
+    assert (produkcije_tuple[ntorka[0]][1][ntorka[1]] in nezavrsniZnakovi)  # nema smisla traziti kontekst ako ispred tocke nije nezavrsni znak
+
+    alpha = desna_strana[ntorka[1] + 1:]
+
+    return kontekst(alpha, ntorka[2])
+
+
+def kontekst(alpha, parent_context):
+    result = zapocinje_alpha(alpha)
+
+    if prazan_skup(alpha):
+        result.update(parent_context)
+
+    return result
+
+
+test = True
+if test: # testovi za "gramatikaPrimjer.txt" iz uputa/materijala
+    assert (kontekst(['a', 'b', 'c'], ['@']) == set(["a"]))
+    assert (kontekst(['<A>', 'b', 'c'], ['@']) == set(["a", "b", "c", "d", "e"]))
+    assert (kontekst(['<B>', 'b', 'c'], ['@']) == set(["b"]))
+    assert (kontekst(['<C>', 'b', 'c'], ['@']) == set(["a", "c", "d", "b"]))
+    assert (kontekst(['<D>', 'b', 'c'], ['@']) == set(["d", "b"]))
+    assert (kontekst(['<E>', 'b', 'c'], ['@']) == set(["c", "e"]))
+    assert (kontekst(['<B>'], ['@']) == set(["b", "@"]))
+
+    assert (odredi_kontekst_ntorke((0, 0, "@")) == set(["@"]))
+    # odredi_kontekst_ntorke((0, 1, "@"))  # ne smi raditi!
+    assert (odredi_kontekst_ntorke((1, 0, "@")) == set(["a", "c", "d"]))
+    # odredi_kontekst_ntorke((1, 2, "@")) # ne smi raditi!
+    assert (odredi_kontekst_ntorke((3, 2, "@")) == set(["c", "e"]))
+    assert (odredi_kontekst_ntorke((4, 1, "@")) == set(["b", "@"]))
+    assert (odredi_kontekst_ntorke((4, 2, "@")) == set(["@"]))
+    assert (odredi_kontekst_ntorke((9, 1, "@")) == set(["f"]))
 
 
 # ispis
@@ -133,6 +256,9 @@ for x in sinkronizacijskiZnakovi:
 out.write("#" * 10 + "\n")
 # for x in produkcije:
 #     out.write(x + "\n")
+
+### TODO
+# - preimenovate stanje_na_produkcije
 
 if __name__ == '__main__':
     pass
