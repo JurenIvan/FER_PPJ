fd0242129c002144b8c1c4f3b7f61e0b36a7c714
Frano Rajič <frano.rajich@gmail.com>
2019-11-29 21:07:21 +0100

    Minor refactoring

--- lab2/GSA.py
+++ lab2/GSA.py
@@ -1,6 +1,5 @@
 from funkcije import *
 
-
 filename = "test.txt"
 # filename = "gramatikaPrimjer.txt"
 fajl = open(filename)
@@ -19,9 +18,9 @@ zavrsni_znakovi = set()
 zavrsni_znakovi.add("$")
 zavrsni_znakovi.update(tmp[1:])
 
-sinkronizacijskiZnakovi = []
+sinkronizacijski_znakovi = []
 tmp = citaj[2].split()
-sinkronizacijskiZnakovi = tmp[1:]
+sinkronizacijski_znakovi = tmp[1:]
 
 # mozda ce trebait preimenovati NoviPocetniNezvarsniZnak
 NNZ = "<NoviNezavrsniZnak>"
@@ -29,35 +28,35 @@ NNZ = "<NoviNezavrsniZnak>"
 stanje_na_produkcije = {}
 # produkcije = []
 produkcije_tuple = []
-trenutniKljuc = ""
+trenutni_kljuc = ""
 citaj.insert(3, NNZ)
 citaj.insert(4, " " + pocetni_nezavrsni_znak)
 for x in citaj[3:]:
     if (x[0] == "<"):
-        trenutniKljuc = x.strip("\n")
+        trenutni_kljuc = x.strip("\n")
         # if (trenutniKljuc not in gramatika.keys()):
         #     gramatika.update({trenutniKljuc: []})
     else:
         # gramatika[trenutniKljuc].append(x.split())
-        lijevaStrana = trenutniKljuc
-        desnaStrana = ""
+        lijeva_strana = trenutni_kljuc
+        desna_strana = ""
         tmp = x.split()
         cnt = 0
         for k in tmp:
-            desnaStrana += k
+            desna_strana += k
             if (cnt < len(tmp) - 1):
-                desnaStrana += ", "
+                desna_strana += ", "
             cnt += 1
 
         # produkcije.append(lijevaStrana + " -> " + desnaStrana)
-        produkcije_tuple.append((lijevaStrana, x.split()))
+        produkcije_tuple.append((lijeva_strana, x.split()))
 
-        if (lijevaStrana not in stanje_na_produkcije.keys()):
-            stanje_na_produkcije[lijevaStrana] = []
-        stanje_na_produkcije[lijevaStrana].append(len(produkcije_tuple) - 1)
+        if (lijeva_strana not in stanje_na_produkcije.keys()):
+            stanje_na_produkcije[lijeva_strana] = []
+        stanje_na_produkcije[lijeva_strana].append(len(produkcije_tuple) - 1)
 
-#print(produkcije_tuple)
-#print(stanje_na_produkcije)
+# print(produkcije_tuple)
+# print(stanje_na_produkcije)
 
 # gramatika[NNZ] = [[pocetni_nezavrsni_znak]]
 
@@ -65,7 +64,6 @@ brojac_stanja = 0
 DKA = []
 stanje = []
 unikati = set()
-
 
 pocetno_stanje = (0, 0, "@")
 stanje.append(pocetno_stanje)
@@ -78,11 +76,11 @@ for x in zavrsni_znakovi:
     if (x != "$"):
         znakovi.append(x)
 
-#print("znakovi su: ", end = "")
-#print(znakovi)
+# print("znakovi su: ", end = "")
+# print(znakovi)
 
-#print(produkcije_tuple)
-#print(stanje)
+# print(produkcije_tuple)
+# print(stanje)
 kreirano_je_novo_stanje = True
 
 neobradene_produkcije = [pocetno_stanje]
@@ -92,7 +90,7 @@ while (neobradene_produkcije != []):
     # ubaci u stanje sve epsilon prijelaze
     for x in stanje:
         produkcija = produkcije_tuple[x[0]]
-        #print("produkcije je {}".format(produkcija))
+        # print("produkcije je {}".format(produkcija))
         if (x[2] == len(produkcija[1])):
             # TODO redukcija je ovo
             continue
@@ -110,11 +108,11 @@ while (neobradene_produkcije != []):
                 neobradene_produkcije.append(ntorka)
 
         stanje = sorted(stanje, key=lambda tup: (tup[0]))
-        #print(stanje)
-        #print(unikati)
-    del(neobradene_produkcije[0])
-        # prije DKA append ide provjera jel to stanje vec postoji 
-        # >> da prodes po tuple[1] i usporedis sa vec postojecima u DKA
+        # print(stanje)
+        # print(unikati)
+    del (neobradene_produkcije[0])
+    # prije DKA append ide provjera jel to stanje vec postoji
+    # >> da prodes po tuple[1] i usporedis sa vec postojecima u DKA
 if (postoji_stanje_u_DKA(stanje, DKA) == False):
     DKA.append((brojac_stanja, stanje))
     brojac_stanja += 1
@@ -123,28 +121,25 @@ if (postoji_stanje_u_DKA(stanje, DKA) == False):
 
 for znak in zavrsni_znakovi - {"$"}:
     for x in stanje:
-        #print("x je {}".format(x))
+        # print("x je {}".format(x))
         br_produkcije = x[0]
         indeks_tocke = x[1]
 
         produkcija_za_prijelaz = produkcije_tuple[br_produkcije]
         iduci_znak_u_produkciji = produkcija_za_prijelaz[1][indeks_tocke]
-        #print("{} {}".format(produkcija_za_prijelaz, iduci_znak_u_produkciji))
+        # print("{} {}".format(produkcija_za_prijelaz, iduci_znak_u_produkciji))
 
         novo_stanje = []
         if (iduci_znak_u_produkciji == znak):
-            #print("jeste: {}".format(iduci_znak_u_produkciji))
-            #print("iduci znak: {}".format(iduci_znak_u_produkciji))
+            # print("jeste: {}".format(iduci_znak_u_produkciji))
+            # print("iduci znak: {}".format(iduci_znak_u_produkciji))
             novo_stanje.append((br_produkcije, indeks_tocke + 1, "@@@"))
-    
+
     if (postoji_stanje_u_DKA(novo_stanje, DKA) == False):
-        #print("NOVO STANJE!")
+        # print("NOVO STANJE!")
         DKA.append((brojac_stanja, novo_stanje))
         brojac_stanja += 1
         kreirano_je_novo_stanje = True
-
-
-
 
 for x in DKA:
     print(x)
@@ -271,7 +266,8 @@ def odredi_kontekst_ntorke(ntorka):
 
     desna_strana = produkcije_tuple[ntorka[0]][1]
     assert (not ntorka[1] > len(desna_strana))  # nesmi tocka bit na kraju, nema smisla tada pozivati..
-    assert (produkcije_tuple[ntorka[0]][1][ntorka[1]] in nezavrsni_znakovi)  # nema smisla traziti kontekst ako ispred tocke nije nezavrsni znak
+    assert (produkcije_tuple[ntorka[0]][1][
+                ntorka[1]] in nezavrsni_znakovi)  # nema smisla traziti kontekst ako ispred tocke nije nezavrsni znak
 
     alpha = desna_strana[ntorka[1] + 1:]
 
@@ -287,7 +283,7 @@ def kontekst(alpha, parent_context):
     return result
 
 
-if filename == "gramatikaPrimjer.txt": # testovi za "gramatikaPrimjer.txt" iz uputa/materijala
+if filename == "gramatikaPrimjer.txt":  # testovi za "gramatikaPrimjer.txt" iz uputa/materijala
     assert (kontekst(['a', 'b', 'c'], ['@']) == set(["a"]))
     assert (kontekst(['<A>', 'b', 'c'], ['@']) == set(["a", "b", "c", "d", "e"]))
     assert (kontekst(['<B>', 'b', 'c'], ['@']) == set(["b"]))
@@ -316,7 +312,7 @@ out.write("#" * 10 + "\n")
 for x in zavrsni_znakovi:
     out.write(x + "\n")
 out.write("#" * 10 + "\n")
-for x in sinkronizacijskiZnakovi:
+for x in sinkronizacijski_znakovi:
     out.write(x + "\n")
 out.write("#" * 10 + "\n")
 # for x in produkcije:
