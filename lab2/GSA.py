f673c640bf3033c73c754fa79577e314b2305b8f
matjuric <matija.juric@hotmail.com>
2019-11-30 23:40:48 +0100

    fixed LR(1) parser tables in output

--- lab2/GSA.py
+++ lab2/GSA.py
@@ -81,54 +81,56 @@ znakovi.append("@")
 znakovi.append(gramatika.pocetni_nezavrsni_znak)
 # print(znakovi)
 
-konacno_stanje_automata = prijelazi_automata[(0, gramatika.pocetni_nezavrsni_znak)]
+# konacno_stanje_automata = prijelazi_automata[(0, gramatika.pocetni_nezavrsni_znak)]
 
-for x in DKA:
-    dohvati_stanje = x[0]
+# for x in DKA:
+#     dohvati_stanje = x[0]
 
-    for znak in znakovi:
-        if ((dohvati_stanje, znak) in prijelazi_automata.keys()):
-            if (znak in gramatika.zavrsni_znakovi):
-                if (znak == "@"):
-                    # prodi po produkcijama stanja i vidi ima li koja produkcija da je na kraju
-                    # i da ima @ u kontekstu
-                    produkcije_stanja = DKA[dohvati_stanje][1]
-                    for k in produkcije_stanja:
-                        pozicija_tocke = k[1]
-                        kontekst = k[2].split()
-                        if (pozicija_tocke == len(gramatika.produkcije[k[0]][1]) - 1):
-                            if (znak in kontekst):
-                                tablica.append(str(dohvati_stanje) + ", " + znak + ", REDUCIRAJ, " + str(k[0]))
-                else:
-                    dohvati_prijelaz = prijelazi_automata[(dohvati_stanje, znak)]
-                    tablica.append(str(dohvati_stanje) + ", " + znak + ", STAVI, " + str(dohvati_prijelaz))
-            elif (znak in gramatika.nezavrsni_znakovi):
-                dohvati_prijelaz = prijelazi_automata[(dohvati_stanje, znak)]
-                tablica.append(str(dohvati_stanje) + ", " + znak + ", POMAKNI, " + str(dohvati_prijelaz))
-        elif (dohvati_stanje == konacno_stanje_automata and znak == "@"):
-            tablica.append(str(dohvati_stanje) + ", " + znak + ", PRIHVATI")
-        else:
-            # provjeri ima li mozda redukcija za taj znak, 
-            #   tj. je li koja produkcija u stanju na kraju i da ima znak u kontekstu
-            # ako nema, samo odbaci NIZ
-            produkcije_stanja = DKA[dohvati_stanje][1]
-            mogu_reducirati = False
-            if (znak in gramatika.nezavrsni_znakovi):
-                for k in produkcije_stanja:
-                    # je li na kraju produkcija
-                    pozicija_tocke = k[1]
-                    kontekst = k[2].split()
-                    if (pozicija_tocke == len(gramatika.produkcije[k[0]][1]) - 1):
-                        if (znak in kontekst):
-                            tablica.append(str(dohvati_stanje) + ", " + znak + ", REDUCIRAJ, " + str(k[0]))
-                            mogu_reducirati = True
-            if (mogu_reducirati == False):
-                tablica.append(str(dohvati_stanje) + ", " + znak + ", ODBACI")
+#     for znak in znakovi:
+#         if ((dohvati_stanje, znak) in prijelazi_automata.keys()):
+#             if (znak in gramatika.zavrsni_znakovi):
+#                 dohvati_prijelaz = prijelazi_automata[(dohvati_stanje, znak)]
+#                 tablica.append(str(dohvati_stanje) + ", " + znak + ", POMAKNI, " + str(dohvati_prijelaz))    
+#             elif (znak in gramatika.nezavrsni_znakovi):
+#                 dohvati_prijelaz = prijelazi_automata[(dohvati_stanje, znak)]
+#                 tablica.append(str(dohvati_stanje) + ", " + znak + ", STAVI, " + str(dohvati_prijelaz))
+                
+#         elif (dohvati_stanje == konacno_stanje_automata and znak == "@"):
+#             tablica.append(str(dohvati_stanje) + ", " + znak + ", PRIHVATI")
+#         else:
+#             # provjeri ima li mozda redukcija za taj znak, 
+#             #   tj. je li koja produkcija u stanju na kraju i da ima znak u kontekstu
+#             # ako nema, samo odbaci NIZ
+#             # print("jeste", znak, dohvati_stanje)
+#             produkcije_stanja = DKA[dohvati_stanje][1]
+#             mogu_reducirati = False
+#             print(produkcije_stanja)
+#             print(znak)
+            
+#             if (znak in gramatika.nezavrsni_znakovi or znak == "@"):
+#                 #print("ETO ME")
+#                 for k in produkcije_stanja:
+#                     # je li na kraju produkcija
+#                     pozicija_tocke = k[1]
+#                     kontekst = k[2].split()
+#                     print("produkcija jeste: ", gramatika.produkcije[k[0]][1])
+#                     print("len jeste: ", len(gramatika.produkcije[k[0]][1]))
+#                     #exit()
+#                     if (pozicija_tocke == len(gramatika.produkcije[k[0]]) - 1):
+#                         if (znak in kontekst):
+#                             #print("JESTE")
+#                             tablica.append(str(dohvati_stanje) + ", " + znak + ", REDUCIRAJ, " + str(k[0]))
+#                             mogu_reducirati = True
+#                             break
+#             if (mogu_reducirati == False):
+#                 tablica.append(str(dohvati_stanje) + ", " + znak + ", ODBACI")
+#             #exit()
 
 
-# for x in tablica:
-#     print(x)
+for x in tablica:
+    print(x)
 
+#print(znakovi)
 # ispis u datoteku "data.txt"
 with open("data.txt", "w") as out:
     for x in gramatika.nezavrsni_znakovi:
@@ -140,15 +142,39 @@ with open("data.txt", "w") as out:
     for x in gramatika.sinkronizacijski_znakovi:
         out.write(x + "\n")
     out.write("#" * 10 + "\n")
-    for x in tablica:
-        out.write(x + "\n")
+    
+    iskoristeni_elementi = []
+    for k, v in prijelazi_automata.items():
+        if k[1] in gramatika.zavrsni_znakovi:
+            out.write("{}, {}, {}, {}\n".format(k[0], k[1], "STAVI", v))
+            iskoristeni_elementi.append((k[0], k[1]))
+        else:
+            out.write("{}, {}, {}, {}\n".format(k[0], k[1], "POMAKNI", v))
+            iskoristeni_elementi.append((k[0], k[1]))
+    for x in DKA:
+        for ntorka in x[1]:
+            produkcija = gramatika.produkcije[ntorka[0]]
+            if produkcija[1][0] == "$" or len(produkcija[1]) == ntorka[1]:
+                for znak in ntorka[2].split():
+                    if ntorka[0] == 0:
+                        out.write("{}, {}, {}\n".format(x[0], znak, "PRIHVATI"))
+                        iskoristeni_elementi.append((x[0], znak))
+                    else:
+                        out.write("{}, {}, {}, {}\n".format(x[0], znak, "REDUCIRAJ", ntorka[0]))
+                        iskoristeni_elementi.append((x[0], znak))
+    #print(iskoristeni_elementi)
+    for x in DKA:
+        for y in znakovi:
+            if ((x[0], y) not in iskoristeni_elementi):
+                out.write("{}, {}, {}\n".format(x[0], y, "ODBACI"))
+
     out.write("#" * 10 + "\n")
     for x in gramatika.produkcije:
         out.write(x[0] + " -> " + ", ".join(x[1]) + "\n")
     out.close()
 
-if __name__ == '__main__':
-    with open("data.txt", "r") as out:
-        print("\n\n")
-        for line in out.readlines():
-            print(line, end="")
+# if __name__ == '__main__':
+#     with open("data.txt", "r") as out:
+#         print("\n\n")
+#         for line in out.readlines():
+#             print(line, end="")
