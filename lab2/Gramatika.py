2e6e4fb058cef5b2b5068764c54f60a824a2d8ea
Frano Rajič <frano.rajich@gmail.com>
2019-11-30 19:19:19 +0100

    Fixed FIRST set calculation

--- lab2/Gramatika.py
+++ lab2/Gramatika.py
@@ -19,7 +19,6 @@ class Gramatika:
         """
         self.nezavrsni_znakovi = lines[0].split()[1:]
         self.pocetni_nezavrsni_znak = self.nezavrsni_znakovi[0]
-        self.__prazni_znakovi = set()
 
         self.zavrsni_znakovi = set()
         self.zavrsni_znakovi.add("$")
@@ -29,10 +28,9 @@ class Gramatika:
 
         self.__init_productions(lines)
 
-        visited = set()
-        for nezavrsni_znak in self.nezavrsni_znakovi:
-            if nezavrsni_znak not in visited:
-                self.__je_li_znak_prazan(nezavrsni_znak, visited)
+        self.__prazni_znakovi = set()
+
+        self.__init_prazni_znakovi(self)
 
         self.__zapocinje = {}
         self.__init_zapocinje(self.pocetni_nezavrsni_znak)
@@ -68,29 +66,42 @@ class Gramatika:
                     self.znak_na_produkcije[lijeva_strana] = []
                 self.znak_na_produkcije[lijeva_strana].append(len(self.produkcije) - 1)
 
-    def __je_li_znak_prazan(self, nezavrsni_znak, visited):
-        if nezavrsni_znak in visited:
-            return nezavrsni_znak in self.__prazni_znakovi
+    def __init_prazni_znakovi(self, self1):
+        moguce_prazne_produkcije = []
+        novi_prazni_znakovi = set()
+        for produkcija in self.produkcije:
+            dodaj = True
+            for znak in produkcija[1]:
+                if znak == "$":
+                    novi_prazni_znakovi.add(produkcija[0])
+                    dodaj = False
+                    break
 
-        for produkcija in self.znak_na_produkcije[nezavrsni_znak]:
-
-            if self.produkcije[produkcija][1][0] == "$":
-                self.__prazni_znakovi.add(nezavrsni_znak)
-                break
-
-            for znak in self.produkcije[produkcija][1]:
                 if znak in self.zavrsni_znakovi:
+                    dodaj = False
                     break
 
-                if znak == nezavrsni_znak:
-                    break
+            if dodaj:
+                moguce_prazne_produkcije.append(produkcija)
 
-                if self.__je_li_znak_prazan(znak, visited):
-                    self.__prazni_znakovi.add(nezavrsni_znak)
-                    break
+        while (len(novi_prazni_znakovi) != 0):
+            self.__prazni_znakovi.update(novi_prazni_znakovi)
+            moguce_prazne_produkcije = [x for x in moguce_prazne_produkcije if x[0] not in novi_prazni_znakovi]
+            novi_prazni_znakovi = set()
 
-        visited.add(nezavrsni_znak)
-        return nezavrsni_znak in self.__prazni_znakovi
+            for i in range(len(moguce_prazne_produkcije)):
+                lijeva_strana = moguce_prazne_produkcije[i][0]
+                desna_strana = moguce_prazne_produkcije[i][1]
+
+                dodaj = True
+                for znak in desna_strana:
+                    if znak not in self.__prazni_znakovi and znak not in novi_prazni_znakovi:
+                        dodaj = False
+                        break
+
+                if dodaj:
+                    if lijeva_strana not in self.__prazni_znakovi:
+                        novi_prazni_znakovi.add(lijeva_strana)
 
     def __prazan_skup(self, alpha):
         """
@@ -113,16 +124,17 @@ class Gramatika:
 
         return self.__prazan_skup(alpha[1:])
 
-    def __init_zapocinje(self, nezavrsni_znak):
+    def __init_zapocinje(self, nezavrsni_znak, already_applied=set()):
         """
         Pomocna metoda koja inicijalizira zapocinje skup danog nezavrsnog znaka
 
         :param nezavrsni_znak:
         :return:
         """
-        if nezavrsni_znak not in self.__zapocinje:
-            self.__zapocinje[nezavrsni_znak] = set()
+        root = len(already_applied) == 0
+        zapocinje = set()
 
+        already_applied.add(nezavrsni_znak)
         for produkcija in self.znak_na_produkcije[nezavrsni_znak]:
             desna_strana = self.produkcije[produkcija][1]
 
@@ -131,16 +143,20 @@ class Gramatika:
                     break
 
                 if znak in self.zavrsni_znakovi:
-                    self.__zapocinje[nezavrsni_znak].add(znak)
+                    zapocinje.add(znak)
                     break
 
-                if znak != nezavrsni_znak:
-                    self.__zapocinje[nezavrsni_znak].update(self.__init_zapocinje(znak))
+                if znak != nezavrsni_znak and znak not in already_applied:
+                    zapocinje.update(self.__init_zapocinje(znak, already_applied))
 
                 if znak not in self.__prazni_znakovi:
                     break
+        already_applied.remove(nezavrsni_znak)
 
-        return self.__zapocinje[nezavrsni_znak]
+        if root and nezavrsni_znak not in self.__zapocinje:
+            self.__zapocinje[nezavrsni_znak] = zapocinje
+
+        return zapocinje
 
     def __zapocinje_alpha(self, alpha):
         """
@@ -198,16 +214,57 @@ class Gramatika:
         return self._kontekst(alpha, ntorka[2].split(" "))
 
 
-if __name__ == '__main__':
+
+def basic_grammar_tests():
     ### TESTING ###
 
-    with open("gramatikaPrimjer.txt") as file:
-        grammar = Gramatika(file.readlines())
+    test1 = """%V <A> <B> <C> <D> <E>
+%T a b c d e f
+%Syn a
+<A>
+ <B> <C> c
+<B>
+ $
+ b <C> <D> <E>
+<A>
+ e <D> <B>
+<C>
+ <D> a <B>
+ c a
+<D>
+ $
+ d <D>
+<E>
+ e <A> f
+ c"""
+
+    test2 = """%V <S> <A> <B> <C> <D>
+%T a b c d
+%Syn d
+<S>
+ <S> <A>
+ a
+<A>
+ <B>
+ <C>
+ d
+<B>
+ $
+ b
+<C>
+ <D>
+ <B>
+ c
+<D>
+ <A>"""
+
+    # TEST 1
+    grammar = Gramatika(test1.splitlines())
 
     assert (grammar._kontekst(['a', 'b', 'c'], ['@']) == {"a"})
     assert (grammar._kontekst(['<A>', 'b', 'c'], ['@']) == {"a", "b", "c", "d", "e"})
-    assert (grammar._kontekst(['<B>', 'b', 'c'], ['@']) == {"b"})
-    assert (grammar._kontekst(['<C>', 'b', 'c'], ['@']) == {"a", "c", "d", "b"})
+    assert (grammar._kontekst(['<B>', 'a', 'c'], ['@']) == {"b", "a"})
+    assert (grammar._kontekst(['<C>', 'b', 'c'], ['@']) == {"a", "c", "d"})
     assert (grammar._kontekst(['<D>', 'b', 'c'], ['@']) == {"d", "b"})
     assert (grammar._kontekst(['<E>', 'b', 'c'], ['@']) == {"c", "e"})
     assert (grammar._kontekst(['<B>'], ['@']) == {"b", "@"})
@@ -221,4 +278,10 @@ if __name__ == '__main__':
     assert (grammar.odredi_kontekst_ntorke((4, 2, "@")) == {"@"})
     assert (grammar.odredi_kontekst_ntorke((9, 1, "@")) == {"f"})
 
+    # TEST 2
+    grammar2 = Gramatika(test2.splitlines())
+    assert grammar2
+
+if __name__ == '__main__':
+    basic_grammar_tests()
     print("All context tests passed!")
