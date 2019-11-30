fe3220f999967d54c08d623c426fb2a96d5df9e3
matjuric <matija.juric@hotmail.com>
2019-11-30 21:08:10 +0100

    added action/new state table

--- lab2/GSA.py
+++ lab2/GSA.py
@@ -1,8 +1,8 @@
 from Gramatika import *
 from funkcije import *
 
-# filename = "test.txt"
-filename = "gramatikaPrimjer.txt"
+filename = "test.txt"
+# filename = "gramatikaPrimjer.txt"
 with open(filename) as f:
     gramatika = Gramatika(f.readlines())
 
@@ -64,17 +64,70 @@ while (nedovrseno_stanje != []):
         prijelazi_automata[(nedovrseno_stanje_indeks, znak)] = indeks_stanja
 
     del (nedovrseno_stanje[0])
-    print(nedovrseno_stanje)
+    #print(nedovrseno_stanje)
+
+# for x in DKA:
+#     print("Stanje ", x[0])
+#     for ntorka in x[1]:
+#         print(ntorka)
+#     print()
+
+# print("PRIJELAZI AUTOMATA")
+# for k, v in prijelazi_automata.items():
+#     print("{} -> {}".format(k, v))
+
+tablica = []
+znakovi.append("@")
+znakovi.append(gramatika.pocetni_nezavrsni_znak)
+# print(znakovi)
+
+konacno_stanje_automata = prijelazi_automata[(0, gramatika.pocetni_nezavrsni_znak)]
 
 for x in DKA:
-    print("Stanje ", x[0])
-    for ntorka in x[1]:
-        print(ntorka)
-    print()
+    dohvati_stanje = x[0]
 
-print("PRIJELAZI AUTOMATA")
-for k, v in prijelazi_automata.items():
-    print("{} -> {}".format(k, v))
+    for znak in znakovi:
+        if ((dohvati_stanje, znak) in prijelazi_automata.keys()):
+            if (znak in gramatika.zavrsni_znakovi):
+                if (znak == "@"):
+                    # prodi po produkcijama stanja i vidi ima li koja produkcija da je na kraju
+                    # i da ima @ u kontekstu
+                    produkcije_stanja = DKA[dohvati_stanje][1]
+                    for k in produkcije_stanja:
+                        pozicija_tocke = k[1]
+                        kontekst = k[2].split()
+                        if (pozicija_tocke == len(gramatika.produkcije[k[0]][1]) - 1):
+                            if (znak in kontekst):
+                                tablica.append(str(dohvati_stanje) + ", " + znak + ", REDUCIRAJ, " + str(k[0]))
+                else:
+                    dohvati_prijelaz = prijelazi_automata[(dohvati_stanje, znak)]
+                    tablica.append(str(dohvati_stanje) + ", " + znak + ", STAVI, " + str(dohvati_prijelaz))
+            elif (znak in gramatika.nezavrsni_znakovi):
+                dohvati_prijelaz = prijelazi_automata[(dohvati_stanje, znak)]
+                tablica.append(str(dohvati_stanje) + ", " + znak + ", POMAKNI, " + str(dohvati_prijelaz))
+        elif (dohvati_stanje == konacno_stanje_automata and znak == "@"):
+            tablica.append(str(dohvati_stanje) + ", " + znak + ", PRIHVATI")
+        else:
+            # provjeri ima li mozda redukcija za taj znak, 
+            #   tj. je li koja produkcija u stanju na kraju i da ima znak u kontekstu
+            # ako nema, samo odbaci NIZ
+            produkcije_stanja = DKA[dohvati_stanje][1]
+            mogu_reducirati = False
+            if (znak in gramatika.nezavrsni_znakovi):
+                for k in produkcije_stanja:
+                    # je li na kraju produkcija
+                    pozicija_tocke = k[1]
+                    kontekst = k[2].split()
+                    if (pozicija_tocke == len(gramatika.produkcije[k[0]][1]) - 1):
+                        if (znak in kontekst):
+                            tablica.append(str(dohvati_stanje) + ", " + znak + ", REDUCIRAJ, " + str(k[0]))
+                            mogu_reducirati = True
+            if (mogu_reducirati == False):
+                tablica.append(str(dohvati_stanje) + ", " + znak + ", ODBACI")
+
+
+# for x in tablica:
+#     print(x)
 
 # ispis u datoteku "out"
 with open("out", "w") as out:
@@ -85,6 +138,9 @@ with open("out", "w") as out:
         out.write(x + "\n")
     out.write("#" * 10 + "\n")
     for x in gramatika.sinkronizacijski_znakovi:
+        out.write(x + "\n")
+    out.write("#" * 10 + "\n")
+    for x in tablica:
         out.write(x + "\n")
     out.write("#" * 10 + "\n")
     for x in gramatika.produkcije:
