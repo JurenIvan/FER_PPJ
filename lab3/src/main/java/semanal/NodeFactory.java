a8f13424222e95e51aa1949ed5feaeaa8eef0add
jurenivan <juren.ivan@gmail.com>
2019-12-22 23:53:35 +0100

    minor fixes

--- lab3/src/main/java/semanal/NodeFactory.java
+++ lab3/src/main/java/semanal/NodeFactory.java
@@ -14,7 +14,7 @@ public class NodeFactory {
 
         nodes.put("<primarni_izraz>", PrimarniIzraz::new);
         nodes.put("<postfiks_izraz>", PostfiksIzraz::new);
-        nodes.put("<lista_argumenata>", ListaDeklaracija::new);
+        nodes.put("<lista_argumenata>", ListaArgumenata::new);
         nodes.put("<unarni_izraz>", UnarniIzraz::new);
         nodes.put("<unarni_operator>", UnarniOperator::new);
         nodes.put("<cast_izraz>", CastIzraz::new);
@@ -29,7 +29,7 @@ public class NodeFactory {
         nodes.put("<bin_ili_izraz>", BinIliIzraz::new);
         nodes.put("<log_i_izraz>", LogIIzraz::new);
         nodes.put("<log_ili_izraz>", LogIliIzraz::new);
-        nodes.put("<izraz_pridruzivanja>", IzrazNaredba::new);
+        nodes.put("<izraz_pridruzivanja>", IzrazPridruzivanja::new);
         nodes.put("<izraz>", Izraz::new);
         nodes.put("<slozena_naredba>", SlozenaNaredba::new);
         nodes.put("<lista_naredbi>", ListaNaredbi::new);
@@ -53,21 +53,7 @@ public class NodeFactory {
     }
 
     static Node create(String input, Node parent) {
-//        if (input.startsWith("<")) {
-//            input.replaceAll(" ", "");
             return nodes.get(input).apply(parent);
-       // }
-//        else {
-//            String[] parts = input.split(" ");
-//            Boolean lIzraz = parts[0].equals("IDN");
-//            if (parts.length > 3) {
-//                parts[2] = input.substring(parts[0].length() + parts[1].length() + 2);
-//            }
-//            //Krivo, unisti ispis
-//            //if (parts[0].equals("ZNAK")){ parts[2] = parts[2].replaceAll("'", "");}
-//            //if (parts[0].equals("NIZ_ZNAKOVA")){ parts[2] = parts[2].replaceAll("\"", "");}
-//            return new UniformniZnak(parts[0], lIzraz, null, Integer.parseInt(parts[1]), 0, parts[2]);
-//        }
     }
 }
 
